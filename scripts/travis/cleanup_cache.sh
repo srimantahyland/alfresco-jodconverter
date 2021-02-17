9555adbdfe9dc380ca3c5b80bca9eedd93dc6040
Alan Davis <alan.davis@alfresco.com>
2021-02-17 15:48:41 +0000

    Build with Travis (#3)
    [no release]

---
+++ scripts/travis/cleanup_cache.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -ev
+
+rm -rf "${HOME}/.m2/repository/org/alfresco/alfresco-jodconverter-core"
---
+++ scripts/travis/maven_release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+# Use full history for release
+git checkout -B "${TRAVIS_BRANCH}"
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+
+# Run the release plugin - with "[skip ci]" in the release commit message
+pushd jodconverter-core
+mvn -B \
+  "-Darguments=-DskipTests" \
+  release:clean release:prepare release:perform \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
+popd
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
+
